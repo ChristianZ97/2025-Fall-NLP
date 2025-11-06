065331d45807172fc843d463edfa3976f0ffbb96
Christian Jeng <kristan970724@gmail.com>
2025-11-06 23:16:57 +0800

    add hidden size

--- HW3/nlp_hw3.py
+++ HW3/nlp_hw3.py
@@ -103,9 +103,9 @@ default_config = {
     "adamw_lr": 0.000144535611723143,
     "alpha": 0.5,
     "weight_decay": 0.0000232078200592346,
-    "dropout_rate": 0.01,
+    "dropout_rate": 0.05,
     "batch_size": 32,
-    "warmup_ratio": 0.1,
+    # "warmup_ratio": 0.1,
 }
 
 wandb.init(
@@ -119,7 +119,7 @@ os.makedirs(save_dir, exist_ok=True)
 # Define the hyperparameters
 # You can modify these values if needed
 # lr = 3e-5
-epochs = 3
+epochs = 4
 train_batch_size = config.batch_size
 validation_batch_size = 256
 
@@ -212,15 +212,15 @@ class MultiLabelModel(torch.nn.Module):
         # hidden_size = self.roberta.config.hidden_size
 
         self.shared_dense = torch.nn.Linear(hidden_size, hidden_size * 2)
-        # self.activation = torch.nn.ReLU()
-        self.activation = torch.nn.GELU()
+        self.activation = torch.nn.ReLU()
+        # self.activation = torch.nn.GELU()
         # self.linear = torch.nn.Linear(hidden_size, hidden_size * 2, bias=False)
         self.dropout = torch.nn.Dropout(config.dropout_rate)
 
         self.regression_head = torch.nn.Sequential(
             torch.nn.Linear(hidden_size * 2, 512),
-            # torch.nn.ReLU(),
-            torch.nn.GELU(),
+            torch.nn.ReLU(),
+            # torch.nn.GELU(),
             torch.nn.Dropout(0.1),
             torch.nn.Linear(512, 1),  # [0, 5]
             torch.nn.Sigmoid(),
@@ -228,8 +228,8 @@ class MultiLabelModel(torch.nn.Module):
 
         self.classification_head = torch.nn.Sequential(
             torch.nn.Linear(hidden_size * 2, 512),
-            # torch.nn.ReLU(),
-            torch.nn.GELU(),
+            torch.nn.ReLU(),
+            # torch.nn.GELU(),
             torch.nn.Dropout(0.1),
             torch.nn.Linear(512, 3),  # 0, 1, 2
         )
@@ -310,20 +310,20 @@ optimizer = [
     torch.optim.AdamW(adamw_params, lr=config.adamw_lr),
 ]
 
-num_training_steps = epochs * len(dl_train)
-num_warmup_steps = int(num_training_steps * config.warmup_ratio)
+# num_training_steps = epochs * len(dl_train)
+# num_warmup_steps = int(num_training_steps * config.warmup_ratio)
 
-scheduler_muon = get_cosine_schedule_with_warmup(
-    optimizer[0],
-    num_warmup_steps=num_warmup_steps,
-    num_training_steps=num_training_steps,
-)
+# scheduler_muon = get_cosine_schedule_with_warmup(
+#    optimizer[0],
+#    num_warmup_steps=num_warmup_steps,
+#    num_training_steps=num_training_steps,
+# )
 
-scheduler_adamw = get_cosine_schedule_with_warmup(
-    optimizer[1],
-    num_warmup_steps=num_warmup_steps,
-    num_training_steps=num_training_steps,
-)
+# scheduler_adamw = get_cosine_schedule_with_warmup(
+#    optimizer[1],
+#    num_warmup_steps=num_warmup_steps,
+#    num_training_steps=num_training_steps,
+# )
 
 # TODO3-2: Define your loss functions (you should have two)
 # Write your code here
@@ -382,8 +382,8 @@ for ep in range(epochs):
         optimizer[0].step()
         optimizer[1].step()
 
-        scheduler_muon.step()
-        scheduler_adamw.step()
+        # scheduler_muon.step()
+        # scheduler_adamw.step()
 
         pbar.set_postfix(loss=loss.item())
         batch_size = batch["input_ids"].shape[0]
