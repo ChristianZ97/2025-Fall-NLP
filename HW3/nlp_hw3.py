6762d1c7ae84829077bcd3801fa823f4ee8af8e9
Christian Jeng <kristan970724@gmail.com>
2025-11-09 22:25:00 +0800

    sweep consis loss

--- HW3/nlp_hw3.py
+++ HW3/nlp_hw3.py
@@ -293,10 +293,7 @@ adamw_params = [
 
 optimizer = [
     SingleDeviceMuon(
-        muon_params,
-        lr=config.muon_lr,
-        weight_decay=config.muon_weight_decay,
-        momentum=config.muon_momentum,
+        muon_params, lr=config.muon_lr, weight_decay=config.muon_weight_decay
     ),
     torch.optim.AdamW(
         adamw_params, lr=config.adamw_lr, weight_decay=config.adamw_weight_decay
