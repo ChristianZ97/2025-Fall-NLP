fc6339f1ab51db6a804290598b00be9120e2400f
Christian Jeng <kristan970724@gmail.com>
2025-11-12 03:05:10 +0800

    try new stuff

--- HW3/Deeper Structure/nlp_hw3.py
+++ HW3/Deeper Structure/nlp_hw3.py
@@ -117,7 +117,7 @@ os.makedirs(save_dir, exist_ok=True)
 # You can modify these values if needed
 # lr = 3e-5
 epochs = 4
-train_batch_size = 32
+train_batch_size = config.batch_size
 validation_batch_size = 256
 
 # TODO1: Create batched data for DataLoader
