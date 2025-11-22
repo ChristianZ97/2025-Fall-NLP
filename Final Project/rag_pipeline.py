0ee86fa7701cfdba3de08797ddd0257d39263135
Christian Jeng <kristan970724@gmail.com>
2025-11-22 18:14:57 +0800

    parallel

--- Final Project/rag_pipeline.py
+++ Final Project/rag_pipeline.py
@@ -17,7 +17,7 @@ from openai import OpenAI
 # Transformers for Embedding & Reranking
 from transformers import AutoModel, AutoTokenizer, AutoModelForSequenceClassification
 
-MAX_DOC_CHARS = 2000
+MAX_DOC_CHARS = 4096
 
 # --- 1. Retrieval Components (Embedding, Indexing, Reranking) ---
 
